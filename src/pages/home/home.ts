23e538b7b51b6af1b75f240067b9371d4c61a886
John Peñaloza <johnmanuelpenaloza@gmail.com>
2018-02-10 23:24:29 -0400

    Ionic juego1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,9 +8,9 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
    mayorMenor:string='...';
    num:number;
-   menPlace="Introdusca un numero del 1 al 100";
-   numberSecret:number=this.numeroAleatorio(0,100);
-
+   menPlace="Introdusca un numero del 1 al 10";
+   numberSecret:number=this.numeroAleatorio(0,10);
+   intentos = 10;
   constructor(public navCtrl: NavController) {
      
   }
@@ -21,19 +21,24 @@ export class HomePage {
   
   compruebaNumero(){
     if(this.num){
-
-      if(this.num < this.numberSecret)
-         this.mayorMenor='menor';
-      else if(this.num > this.numberSecret)
-         this.mayorMenor='mayor';
-      else
-         this.mayorMenor='igual';
-
+      if(this.num < this.numberSecret){
+        this.intentos=this.intentos-1;
+        this.mayorMenor='menor';
+      }else if(this.num > this.numberSecret){
+        this.intentos=this.intentos-1;
+        this.mayorMenor='mayor';
+      }else{
+        this.mayorMenor='igual';
+        (this.intentos<10)?this.intentos:this.intentos+1;
+      }
     }
   }
  
   reiniciar(){
     this.mayorMenor='...';
+    this.intentos=10;
+    this.numberSecret=this.numeroAleatorio(0,10);
+    this.num=0;
   }
 
 }
